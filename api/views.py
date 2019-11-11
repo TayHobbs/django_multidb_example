4ac3de9536c97e54d05984291cc1c0543e1544f6
TayHobbs <taylor.hobbs@imtins.com>
2019-11-11 09:11:27 -0600

    Example complete

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework_json_api.views import ModelViewSet
+from rest_framework_json_api import serializers
 
-# Create your views here.
+from api.models import Example
+
+
+class ExampleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Example
+        fields = '__all__'
+
+
+class ExampleViewSet(ModelViewSet):
+    serializer_class = ExampleSerializer
+
+    def get_queryset(self):
+        using = self.request.query_params.get('using', 'default')
+        return Example.objects.using(using).all()
+
